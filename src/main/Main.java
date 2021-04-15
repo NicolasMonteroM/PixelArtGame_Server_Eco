4fe3ba0d15f6aab54aa383676f41284cde5c7a1d
NicolasMonteroM <nicolasmontero42@gmail.com>
2021-04-15 12:13:54 -0500

    Initial structure

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+
+	}
+	
+	public void settings() {
+
+		size(1200, 700);
+
+	}
+	
+	public void setup() {
+		
+		
+	}
+	
+	public void draw() {
+		
+		
+	}
+
+}
